1e0ae5cef33b78720db60ec41bbb809c4fd64890
“ksigei” <sigeikiprono4@gmail.com>
2022-09-13 22:27:00 +0300

    Create a simple blog using rails

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#detail"
+  resources :articles
+  get '/about', to: 'about#about'
+  # resources :about
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+end
